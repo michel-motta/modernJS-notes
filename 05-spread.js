49ad68a9f6f169e300c8a394a6b7b78a774499f0
Michel Motta <michelmotta.dev@gmail.com>
2021-12-21 21:32:13 -0300

    spread operator update

--- 05-spread.js
+++ 05-spread.js
@@ -1,2 +1,22 @@
 // SPREAD OPERATOR
 
+const user = {
+  firstName: 'Michel',
+  lastName: 'Motta',
+  age: 24,
+  instagram: '@msvmotta',
+  skills: ['HTML', 'CSS', 'JavaScript', 'NodeJS'],
+  active: false,
+};
+
+// a sintaxe é igual o rest operator, porém é usado somente dentro de um objeto
+const updatedUser = {
+  ...user,
+  skills: ['TypeScript', ...user.skills],
+  active: true,
+};
+
+
+console.log(user.skills);
+
+console.log(updatedUser);
