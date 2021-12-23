557c6dcdd55c8a1febbf610bb861a3928e566885
Michel Motta <michelmotta.dev@gmail.com>
2021-12-22 23:10:10 -0300

    add template strings

---
+++ 06-templatestring.js
@@ -0,0 +1,7 @@
+// TEMPLATE STRING
+
+// permite a interpolação de expressões javascript, além de identificar quebra de linhas
+
+let userName = 'Michel';
+// tudo entre {} é javascript, inclusive operações matemáticas, variáveis, etc
+console.log(`Seja bem-vindo ${userName}!`);
